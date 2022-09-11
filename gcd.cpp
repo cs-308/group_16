20faf3840cee2cda1c26115d865eea8f2935696d
Silverback-14 <anatharam.vasanth@gmail.com>
2022-09-11 21:58:04 +0530

    // Commited to branch

--- gcd.cpp
+++ gcd.cpp
@@ -12,12 +12,30 @@ int gcd(int a, int b)
     }
     return result; 
 }
+int gcdR(int a, int b)
+{
+	
+	if (a == 0)
+	return b;
+	if (b == 0)
+	return a;
+
+	
+	if (a == b)
+		return a;
+
+
+	if (a > b)
+		return gcdR(a-b, b);
+	return gcdR(a, b-a);
+}
+
  
 int main()
 {
     int a,b;
     cin>>a>>b;
     cout << "GCD of " << a << " and " << b << " is "
-         << gcd(a, b);
+         << gcd(a, b)<<gcdR(a, b);
     return 0;
 }
